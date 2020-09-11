86208854277fea8aa8c98b9c8aa2f02ff6f11b58
david-forer <david.j.forer@gmail.com>
2020-09-11 15:17:24 +0900

    finished search

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import algoliasearch from 'algoliasearch/lite';
+import { InstantSearch, SearchBox, Hits, Pagination, Stats } from 'react-instantsearch-dom';
 import './App.css';
 
 // ===== OPTION 1: if you want to create your own algolia account and fill it with data =====
@@ -10,10 +12,38 @@ import './App.css';
 // api key: ae8a3f7cff93d40282a4831930bfa38b
 // index name: products
 
+
+const searchClient = algoliasearch(
+  'S90CEM4XMP',
+  '22568000f94b220e67dc014a3d6bd9da'
+);
+
+
 export default function App() {
   return (
-    <div className="app">
-      <div className="search-container"></div>
-    </div>
+    <InstantSearch searchClient={searchClient} indexName="products">
+      <div className="app">
+        <div className="search-container">
+          <Stats />
+          <SearchBox />
+          <Hits hitComponent={Product} />
+          <Pagination />
+        </div>
+      </div>
+    </InstantSearch>
   );
+}
+
+function Product({ hit }) {
+  return (
+    <a className="product" href={hit.url} target="_blank" rel="noopener noreferrer">
+      <img src={hit.image} alt={hit.name} />
+      <div>
+        <h3>{hit.brand}</h3>
+        <h2>{hit.name}</h2>
+        <p>{hit.price}</p>
+      </div>
+    </a>
+  );
+  
 }
